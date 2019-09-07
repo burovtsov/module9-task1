1da708be48f3d5d4917b20519915c467d9d31726
burovtsov <burovcov@gmail.com>
2019-09-07 18:01:15 +0300

    added methods of Reader

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    Reader reader = new Reader();
+
+    System.out.println("Говорите точно, сколько вешать в граммах?");
+    System.out.println("Например: C:/Windows/System32");
+
+
+    for(;;) {
+        System.out.print("Путь к папке для взвешивания >> ");
+        String path = scanner.nextLine();
+        reader.folderSize(path);
+    }
+    }
 }
--- src/main/java/Reader.java
+++ src/main/java/Reader.java
@@ -1,4 +1,58 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Reader {
+    private double summary;
+    private int directoryCount;
+    private String path;
+    private List<File> inFolder = new ArrayList<>();
+    private List<File> directories = new ArrayList<>();
+    private List<File> files = new ArrayList<>();
+
+    public void folderSize(String path) {
+        this.path = path;
+        sizeCalculator(folderList(path));
+        sizeToString();
+    }
+
+    private List<File> folderList(String path) {
+        inFolder.addAll(Arrays.asList(new File(path).listFiles()));
+        fileOrDirectory();
+
+        for (int i = 0; i < directoryCount; i++) {
+           inFolder.removeAll(inFolder);
+           inFolder.addAll(Arrays.asList(directories.get(i).listFiles()));
+           fileOrDirectory();
+       }
+        return files;
+    }
+
+    private void fileOrDirectory() {
+        for (File f : inFolder) {
+            if (f.isDirectory()) {
+                directoryCount++;
+                directories.add(f);
+            } else
+                files.add(f);
+        }
+    }
+
+    private double sizeCalculator(List<File> files) {
+        for(File f : files)
+            summary += f.length();
+        return summary;
+    }
+
+    private void sizeToString() {
+        if (summary < 1024)
+            System.out.format("Папка " + path + " занимает %f.2%n" + summary + " Байт");
+        else if (summary < 1024 * 1024)
+            System.out.format("Папка " + path + " занимает %f.2%n" + summary/1024 + " Кбайт");
+        else if (summary < 1024 * 1024 * 1024)
+            System.out.format("Папка " + path + " занимает %f.2%n" + summary/1024/1024 + " Mб");
+        else if (summary < 1024 * 1024 * 1024 * 1024)
+            System.out.format("Папка " + path + " занимает %f.2%n" + summary/1024/1024/1024 + " Гб");
+    }
 }
